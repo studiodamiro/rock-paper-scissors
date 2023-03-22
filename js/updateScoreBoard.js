55619f802d707fbac30856dedfee45987ff6d5af
damiro <hello.damiro@gmail.com>
2023-03-23 00:56:40 +0800

    change bit of text when score updates

--- js/updateScoreBoard.js
+++ js/updateScoreBoard.js
@@ -7,11 +7,11 @@ export function updateScoreBoard(playerChoice, computerChoice) {
     document.querySelector('.opponent-text').innerHTML = computerChoice;
 
     if (roundStatus === true) {
-        document.querySelector('.opponent-side > h3').innerHTML = 'loser';
-        document.querySelector('.player-side > h3').innerHTML = 'winner';
+        document.querySelector('.opponent-side > h3').innerHTML = 'you lose';
+        document.querySelector('.player-side > h3').innerHTML = 'you win';
     } else if (roundStatus === false) {
-        document.querySelector('.opponent-side > h3').innerHTML = 'winner';
-        document.querySelector('.player-side > h3').innerHTML = 'loser';
+        document.querySelector('.opponent-side > h3').innerHTML = 'you win';
+        document.querySelector('.player-side > h3').innerHTML = 'you lose';
     } else {
         document.querySelector('.opponent-side > h3').innerHTML = 'draw';
         document.querySelector('.player-side > h3').innerHTML = 'draw';
