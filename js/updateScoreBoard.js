386a36a05689081c15a2ee28ad3e474117965f37
damiro <hello.damiro@gmail.com>
2023-03-23 00:49:27 +0800

    placed playRound at updateScoreBoard to confirm who won the round

--- js/updateScoreBoard.js
+++ js/updateScoreBoard.js
@@ -1,4 +1,19 @@
+import { playRound } from './gameAlgorithm.js';
+
 export function updateScoreBoard(playerChoice, computerChoice) {
+    let roundStatus = playRound(playerChoice, computerChoice);
+
     document.querySelector('.player-text').innerHTML = playerChoice;
     document.querySelector('.opponent-text').innerHTML = computerChoice;
+
+    if (roundStatus === true) {
+        document.querySelector('.opponent-side > h3').innerHTML = 'loser';
+        document.querySelector('.player-side > h3').innerHTML = 'winner';
+    } else if (roundStatus === false) {
+        document.querySelector('.opponent-side > h3').innerHTML = 'winner';
+        document.querySelector('.player-side > h3').innerHTML = 'loser';
+    } else {
+        document.querySelector('.opponent-side > h3').innerHTML = 'draw';
+        document.querySelector('.player-side > h3').innerHTML = 'draw';
+    }
 }
