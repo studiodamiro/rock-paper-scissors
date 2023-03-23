0ea1c1358d834a564d3d4053e99ba216abb6d1d9
damiro <hello.damiro@gmail.com>
2023-03-23 15:00:06 +0800

    added a function to update scoreboard. updateScoreDisplay

--- js/updateScoreBoard.js
+++ js/updateScoreBoard.js
@@ -9,11 +9,25 @@ export function updateScoreBoard(playerChoice, computerChoice) {
     if (roundStatus === true) {
         document.querySelector('.opponent-side > h3').innerHTML = 'you lose';
         document.querySelector('.player-side > h3').innerHTML = 'you win';
+        updateScoreDisplay(true);
     } else if (roundStatus === false) {
         document.querySelector('.opponent-side > h3').innerHTML = 'you win';
         document.querySelector('.player-side > h3').innerHTML = 'you lose';
+        updateScoreDisplay(false);
     } else {
         document.querySelector('.opponent-side > h3').innerHTML = 'draw';
         document.querySelector('.player-side > h3').innerHTML = 'draw';
+    }
+}
+
+function updateScoreDisplay(win) {
+    if (win) {
+        let playerScore = document.querySelector('.score h3:nth-of-type(2)').innerHTML;
+        playerScore++;
+        document.querySelector('.score h3:nth-of-type(2)').innerHTML = playerScore;
+    } else {
+        let opponentScore = document.querySelector('.score h3:nth-of-type(1)').innerHTML;
+        opponentScore++;
+        document.querySelector('.score h3:nth-of-type(1)').innerHTML = opponentScore;
     }
 }
