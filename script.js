31d2ac1ab3a76fe24999441a567e5e8a7509d0a6
damiro <hello.damiro@gmail.com>
2023-03-24 14:59:18 +0800

    finalize raceToNumber variable for final deployment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { helpRenderCards } from './js/helpRenderCards.js';
 import { setHoverAndClickOnCards } from './js/hoverAndClickOnCards.js';
 import { prepareGame } from './js/startGame.js';
 
-export const raceToNumber = 2;
+export const raceToNumber = 5;
 
 export let playerScore = 0;
 export let setPlayerScore = (newValue) => (playerScore = newValue);
